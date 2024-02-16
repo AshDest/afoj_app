2c076935b2e2d70ebc1390b3881e0fc66e3113d1
AshDest <ashuzamaheshe4@gmail.com>
2024-02-16 12:08:29 +0200

    ash commit

--- resources/views/pages/sections/_about.blade.php
+++ resources/views/pages/sections/_about.blade.php
@@ -25,16 +25,21 @@
 
             <div class="col-md-6 mt-4 pt-2 mt-sm-0 pt-sm-0">
                 <div class="section-title ms-lg-5">
-                    <span class="badge bg-soft-primary text-uppercase text-primary py-2 px-3 rounded-pill mb-2">get
-                        to know us</span>
-                    <h3 class="title text-capitalize mb-4">About Us</h3>
-                    <p class="text-muted">We are so excited and proud of our business. It's really easy to create a
-                        landing page for your awesome business.</p>
-                    <p class="text-muted">Phasellus ut condimentum diam, eget tempus lorem. Morbi bibendum est quis
-                        arcu posuere condimentum. Nullam justo eros, pellentesque a metus non, mattis volutpat
-                        libero. Praesent ornare felis ante, sit amet porta felis pellentesque fermentum.</p>
+                    <span class="badge bg-soft-primary text-uppercase text-primary py-2 px-3 rounded-pill mb-2">About
+                        us</span>
+                    <h3 class="title text-capitalize mb-4">A Propos de nous</h3>
+                    <p class="text-muted">Un rassemblement d’hommes et des femmes congolais qui croyons que pour
+                        construire une Nation forte et solide, il faut commencer par construire des
+                        familles fortes et solides.</p>
+                    <p class="text-muted">Des familles où les futurs dirigeants des Nations sont
+                        protégés et formés pour créer des sociétés plus humaines, plus résilientes.
+                    </p>
+                    <p class="text-muted">Nous croyons que la violence et la misère en RDC est principalement dû à la
+                        crise des valeurs humaines et la misère dans les familles, en commençant par la
+                        façon dont les futurs leaders sont y traités.
+                    </p>
                     <div class="mt-3 pt-2">
-                        <a href="page-about.html" class="btn btn-primary">Read more</a>
+                        <a href="page-about.html" class="btn btn-primary">Lire plus</a>
                     </div>
                 </div>
             </div>
--- resources/views/pages/sections/_slide.blade.php
+++ resources/views/pages/sections/_slide.blade.php
@@ -5,13 +5,14 @@
         <div class="row justify-content-center mt-5">
             <div class="col-lg-6 col-12">
                 <div class="title-heading text-center">
-                    <h1 class="heading text-white mb-4">We Are Helping To Grow Your
-                        Business !
+                    <h1 class="heading text-white mb-4">Association Fondation Jamaa,
+                        (AFOJ)
                     </h1>
-                    <p class="text-white-50 mx-auto para-desc">Are you looking for professional advice & proper
-                        service for your project ? Digital, business & technology consulting
-                        for business resilience.</p>
-                    <a href="#" class="btn btn-primary pt-2 mt-4">Contact Us </a>
+                    <p class="text-white-50 mx-auto para-desc">Un rassemblement d’hommes et des femmes congolais qui
+                        croyons que pour
+                        construire une Nation forte et solide, il faut commencer par construire des
+                        familles fortes et solides.</p>
+                    <a href="#" class="btn btn-primary pt-2 mt-4">Lire plus </a>
                 </div>
             </div>
             <!-- end col -->
