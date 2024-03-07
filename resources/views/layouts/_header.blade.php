d9527b684785568fb6a4f31584383e1e3e52846e
AshDest <ashuzamaheshe4@gmail.com>
2024-03-07 22:15:02 +0200

    ash commit

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,142 +1,34 @@
-    <header id="topnav" class="defaultscroll sticky">
-        <div class="container">
-            <!-- Logo container-->
-            <a class="logo" href="/">
-                <span class="logo-light-mode">
-                    <img src="{{ asset('assets/images/logo-light_2.png') }}" class="l-dark" alt="">
-                    <img src="{{ asset('assets/images/logo-light_2.png') }}" class="l-light" alt="">
-                </span>
-                <img src="{{ asset('assets/images/logo-light_2.png') }}" class="logo-dark-mode" alt="">
-            </a>
-            <!-- End Logo container-->
-            <div class="menu-extras">
-                <div class="menu-item">
-                    <!-- Mobile menu toggle-->
-                    <a class="navbar-toggle" id="isToggle" onclick="toggleMenu()">
-                        <div class="lines">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </a>
-                    <!-- End mobile menu toggle-->
+<div class="toolbar-area topbar-style1 hidden-md">
+    <div class="container">
+        <div class="row rs-vertical-middle">
+           <div class="col-lg-8">
+                <div class="toolbar-contact">
+                    <ul class="rs-contact-info">                   
+                        <li>
+                            <i class="fi fi-rr-envelope-plus"></i>                  
+                            <a href="mailto:info@renovix.biz">info@renovix.biz</a>               
+                        </li>
+                        <li>
+                              <i class="fi fi-rr-phone-call"></i>                                      
+                            <a href="tel:+14888444999"> +14888444999</a>                   
+                        </li>
+                        <li>
+                              <i class="fi-rr-map-marker-home"></i>                                      
+                            503 Ocala Street, Florida, USA                
+                        </li>
+                    </ul>
                 </div>
             </div>
-
-            <ul class="buy-button list-inline mb-0">
-                <li class="list-inline-item search-icon mb-0">
-                    <div class="dropdown">
-                        <button type="button" class="btn btn-link text-decoration-none dropdown-toggle mb-0 p-0"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="uil uil-search h5 text-dark nav-light-icon-dark mb-0"></i>
-                            <i class="uil uil-search h5 text-white nav-light-icon-white mb-0"></i>
-                        </button>
-                        <div class="dropdown-menu dd-menu dropdown-menu-end bg-white shadow rounded border-0 mt-4 py-0"
-                            style="width: 300px;">
-                            <form class="p-4">
-                                <input type="text" id="text" name="name" class="form-control border bg-white"
-                                    placeholder="Search...">
-                            </form>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-
-            <div id="navigation">
-                <!-- Navigation Menu-->
-                <ul class="navigation-menu nav-light nav-right">
-                    <li class="has-submenu parent-parent-menu-item">
-                        <a href="/">Home</a><span class="menu-arrow"></span>
-                    </li>
-                    <li><a href="#" class="sub-menu-item">About Us</a></li>
-
-                    <li class="has-submenu parent-parent-menu-item">
-                        <a href="javascript:void(0)">Evenement</a><span class="menu-arrow"></span>
-                        <ul class="submenu megamenu">
-                            <li>
-                                <ul>
-                                    <li class="megamenu-head">Grid Portfolio</li>
-                                    <li><a href="portfolio-grid-two.html" class="sub-menu-item">Two Column</a></li>
-                                    <li><a href="portfolio-grid-three.html" class="sub-menu-item">Three Column</a>
-                                    </li>
-                                    <li><a href="portfolio-grid-four.html" class="sub-menu-item">Four Column</a></li>
-                                    <li><a href="portfolio-grid-five.html" class="sub-menu-item">Five Column</a></li>
-                                    <li><a href="portfolio-grid-six.html" class="sub-menu-item">Six Column</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li class="megamenu-head">Modern Portfolio</li>
-                                    <li><a href="portfolio-modern-two.html" class="sub-menu-item">Two Column</a></li>
-                                    <li><a href="portfolio-modern-three.html" class="sub-menu-item">Three Column</a>
-                                    </li>
-                                    <li><a href="portfolio-modern-four.html" class="sub-menu-item">Four Column</a>
-                                    </li>
-                                    <li><a href="portfolio-modern-five.html" class="sub-menu-item">Five Column</a>
-                                    </li>
-                                    <li><a href="portfolio-modern-six.html" class="sub-menu-item">Six Column</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li class="megamenu-head">Masonry Portfolio</li>
-                                    <li><a href="portfolio-masonry-two.html" class="sub-menu-item">Two Column</a></li>
-                                    <li><a href="portfolio-masonry-three.html" class="sub-menu-item">Three Column</a>
-                                    </li>
-                                    <li><a href="portfolio-masonry-four.html" class="sub-menu-item">Four Column</a>
-                                    </li>
-                                    <li><a href="portfolio-masonry-five.html" class="sub-menu-item">Five Column</a>
-                                    </li>
-                                    <li><a href="portfolio-masonry-six.html" class="sub-menu-item">Six Column</a></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <ul>
-                                    <li class="megamenu-head">Standard Portfolio</li>
-                                    <li><a href="portfolio-st-two.html" class="sub-menu-item">Two Column</a></li>
-                                    <li><a href="portfolio-st-three.html" class="sub-menu-item">Three Column</a></li>
-                                    <li><a href="portfolio-st-four.html" class="sub-menu-item">Four Column</a></li>
-                                    <li><a href="portfolio-st-five.html" class="sub-menu-item">Five Column</a></li>
-                                    <li><a href="portfolio-st-six.html" class="sub-menu-item">Six Column</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li class="megamenu-head">Portfolio Deatils</li>
-                                    <li><a href="portfolio-one.html" class="sub-menu-item"> Portfolio One</a></li>
-                                    <li><a href="portfolio-two.html" class="sub-menu-item"> Portfolio Two</a></li>
-                                    <li><a href="portfolio-three.html" class="sub-menu-item"> Portfolio Three</a></li>
-                                    <li><a href="portfolio-four.html" class="sub-menu-item"> Portfolio Four</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="has-submenu parent-parent-menu-item">
-                        <a href="javascript:void(0)">Publications</a><span class="menu-arrow"></span>
-                        <ul class="submenu">
-                            <li><a href="blog-two-column.html" class="sub-menu-item">Blog 2 column</a></li>
-                            <li><a href="blog-three-column.html" class="sub-menu-item">Blog 3 column</a></li>
-                            <li><a href="blog-leftsidebar.html" class="sub-menu-item">Left Sidebar</a></li>
-                            <li><a href="blog-rightsidebar.html" class="sub-menu-item">Right Sidebar</a></li>
-                            <li><a href="blog-fullwidth.html" class="sub-menu-item">Full width</a></li>
-                            <li><a href="blog-detail.html" class="sub-menu-item"> Blog detail</a></li>
-                            <li><a href="blog-author.html" class="sub-menu-item">Author</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="has-submenu parent-menu-item"><a href="javascript:void(0)">Contact</a><span
-                            class="submenu-arrow"></span>
-                        <ul class="submenu">
-                            <li><a href="page-contact-one.html" class="sub-menu-item">Contact One</a></li>
-                            <li><a href="page-contact-two.html" class="sub-menu-item">Contact Two</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <!--end navigation menu-->
+            <div class="col-lg-4">
+                <div class="toolbar-sl-share">
+                    <ul class="clearfix">
+                        <li class="opening"> <em><i class="fi fi-rr-time-add"></i> Mon - Sat: 9.30 to 16.00</em> </li>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
             </div>
-            <!--end navigation-->
         </div>
-        <!--end container-->
-    </header>
+    </div>
+</div>
---
+++ resources/views/layouts/_menus.blade.php
@@ -0,0 +1,155 @@
+<div class="menu-area menu-sticky">
+    <div class="container">
+        <div class="row-table">
+            <div class="col-cell header-logo">                                  
+                <div class="logo-area">
+                    <a href="index.html">
+                        <img class="normal-logo" src="assets/images/logo-dark.png" alt="logo">  
+                        <img class="sticky-logo" src="assets/images/logo-dark.png" alt="logo">
+                    </a>
+                </div>
+            </div>
+            <div class="col-cell">
+                <div class="rs-menu-area">
+                    <div class="main-menu">
+                        <nav class="rs-menu hidden-md">
+                            <ul class="nav-menu">
+                                <li class="menu-item-has-children current-menu-item">
+                                    <a href="index.html">Home</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item-has-children current-menu-item">
+                                            <a href="index.html">Multipages <i class="fi fi-rr-plus-small"></i></a>
+                                            <ul class="sub-menu">
+                                                <li class="active"><a href="index.html">Home 1</a></li>
+                                                <li><a href="index2.html">Home 2</a></li>
+                                                <li><a href="index3.html">Home 3</a></li>
+                                                <li><a href="index4.html">Home 4</a></li>
+                                                <li><a href="index5.html">Home 5</a></li>
+                                            </ul> 
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a href="onepage1.html">Onepages <i class="fi fi-rr-plus-small"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="onepage1.html">Onepage 1</a></li>
+                                                <li><a href="onepage2.html">Onepage 2</a></li>
+                                                <li><a href="onepage3.html">Onepage 3</a></li>
+                                                <li><a href="onepage4.html">Onepage 4</a></li>
+                                                <li><a href="onepage5.html">Onepage 5</a></li>
+                                            </ul>  
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="services1.html">Services</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="services1.html">Services 1</a></li>
+                                        <li><a href="services2.html">Services 2</a></li>
+                                        <li class="last-item menu-item-has-children">
+                                            <a href="business-planning.html">Services Single <i class="fi fi-rr-plus-small"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="business-planning.html">Business Planning</a></li>
+                                                <li><a href="financial-advices.html">Financial Advices</a></li>
+                                                <li><a href="business-analysis.html">Business Analysis</a></li>
+                                                <li><a href="reports-analysis.html">Reports Analysis</a></li>
+                                                <li><a href="fintech-analysis.html">Fintech Analysis</a></li>
+                                                <li><a href="project-reporting.html">Project Reporting</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Pages</a>
+                                    <ul class="sub-menu">
+                                        <li class="last-item menu-item-has-children">
+                                            <a href="team-style1.html">Team <i class="fi fi-rr-plus-small"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="team-style1.html">Team Style 1</a></li>
+                                                <li><a href="team-style2.html">Team Style 2</a></li>
+                                                <li><a href="team-style3.html">Team Style 3</a></li>
+                                                <li><a href="team-style4.html">Team Style 4</a></li>
+                                                <li><a href="team-single.html">Team Single</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="last-item menu-item-has-children">
+                                            <a href="portfolios-style1.html">Portfolios <i class="fi fi-rr-plus-small"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="portfolios-style1.html">Portfolios Style 1</a></li>
+                                                <li><a href="portfolios-style2.html">Portfolios Style 2</a></li>
+                                                <li><a href="portfolios-style3.html">Portfolios Style 3</a></li>
+                                                <li><a href="portfolios-style4.html">Portfolios Style 4</a></li>
+                                                <li><a href="portfolios-style5.html">Portfolios Style 5</a></li>
+                                                <li><a href="portfolios-style6.html">Portfolios Style 6</a></li>
+                                                <li><a href="portfolios-filter.html">Portfolios Filter</a></li>
+                                                <li><a href="portfolios-single.html">Portfolios Single</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="last-item menu-item-has-children">
+                                            <a href="shop.html">Shop <i class="fi fi-rr-plus-small"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="shop.html">Shop</a></li>
+                                                <li><a href="shop-single.html">Shop Single</a></li>
+                                                <li><a href="cart.html">Cart</a></li>
+                                                <li><a href="checkout.html">Checkout</a></li>
+                                                <li><a href="my-account.html">My Account</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="appointment.html">Appointment</a></li>
+                                        <li class="last-item menu-item-has-children">
+                                            <a href="careers.html">Careers <i class="fi fi-rr-plus-small"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="careers.html">Careers</a></li>
+                                                <li><a href="careers-single.html">Careers Single</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="partners.html">Partners</a></li>
+                                        <li><a href="pricing-plans.html">Pricing Plans</a></li>
+                                        <li><a href="faq.html">Faq</a></li>     
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="blog.html">Blog</a>                                                        
+                                    <ul class="sub-menu">  
+                                        <li><a href="blog.html">Blog</a></li>
+                                        <li><a href="blog-single.html"> Blog Single</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="contact1.html">Contact</a>                                                        
+                                    <ul class="sub-menu">  
+                                        <li><a href="contact1.html">Contact 1</a></li>
+                                        <li><a href="contact2.html">Contact 2</a></li>
+                                    </ul>
+                                </li>
+                            </ul> <!-- //.nav-menu -->
+                        </nav>
+                    </div> <!-- //.main-menu -->
+                </div>
+            </div>
+            <div class="col-cell">
+                <div class="expand-btn-inner">
+                    <ul>
+                        <li class="btn-quote"><a class="readon slide-started" href="contact1.html">Get a Quote</a></li>
+                        <li class="humburger">
+                            <a id="nav-expander" class="nav-expander bar" href="#">
+                                <div class="bar">
+                                    <span class="dot1"></span>
+                                    <span class="dot2"></span>
+                                    <span class="dot3"></span>
+                                    <span class="dot4"></span>
+                                    <span class="dot5"></span>
+                                    <span class="dot6"></span>
+                                    <span class="dot7"></span>
+                                    <span class="dot8"></span>
+                                    <span class="dot9"></span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
