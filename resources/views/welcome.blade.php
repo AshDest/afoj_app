8cf4047120d7c84c9cce0cae2eed60b05ced22d5
AshDest <ashuzamaheshe4@gmail.com>
2024-03-08 10:53:01 +0200

    Ash commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,17 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="content-wrap">
-                        <span class="sub-text">Welcome To Renovix</span>
-                        <h1 class="title">We Help Building<br> Your<span> Business</span></h1>
+                        <span class="sub-text">Bienvenue sur AFOJ</span>
+                        <h1 class="title">Association<br> Fondation<span> Jamaa</span></h1>
                         <p class="description">
-                            We are leading Business Consulting company all over the<br>
-                            world doing over 40 years.
+                            Nous accompagnons des familles
+                            congolaises à un meilleur
+                            <br>
+                            traitement des enfants en leur
+                            sein depuis plus de 10 ans.
                         </p>
                         <div class="banner-btn mt-40 md-mt-25">
-                            <a class="readon slide-started" href="contact1.html"><span>Get Started</span></a>
+                            <a class="readon slide-started" href="contact1.html"><span>Voir plus</span></a>
                         </div>
                         <div class="banner-box banner-boxs"></div>
                     </div>
