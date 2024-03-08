a3e7e59b4f6ea75fb71c41310c1a21b499bf0dd4
AshDest <ashuzamaheshe4@gmail.com>
2024-03-08 17:01:22 +0200

    asj commit

--- resources/views/pages/sections/_approch.blade.php
+++ resources/views/pages/sections/_approch.blade.php
@@ -3,26 +3,30 @@
         <div class="row y-middle">
             <div class="col-lg-6 pr-65 md-pr-15 md-mb-50">
                 <div class="sec-title mb-30">
-                    <span class="sub-text big-text">Our Approach</span>
+                    <span class="sub-text big-text">Notre Approche</span>
                     <h2 class="title title2 title-color pb-20">
-                        Building New Generation
-                        Skillfull &<span> Productive</span>
+                        Bienveillance,
+                        Respect &<span> rigeur.</span>
                     </h2>
-                    <p class="desc small2">Leverage agile frameworks to provide a robust synopsis for high level
-                        overviews. Iterative approaches to corporate strategy data foster to collaborative thinking to
-                        empowerment.</p>
+                    <p class="desc small2">Les attitudes positives : compréhension mutuelle,
+                        l’écoute, le respect des sensibilités et de la
+                        personnes de l’autre, le rire, le sourire, demander
+                        pardon, dire merci, dire je t’en prie, éviter de
+                        condamner, de juger les personnes, dire je t’aime,
+                        essayer, se déterminer pour le bonheur de l’autre,
+                        donner pour le bonheur de donner, compatir…</p>
                 </div>
                 <div class="row">
                     <div class="col-lg-6">
                         <ul class="check-lists">
-                            <li>Strategy & Planning</li>
-                            <li>Audit & Evaluation</li>
+                            <li>Sensibilisation</li>
+                            <li>Formation</li>
                         </ul>
                     </div>
                     <div class="col-lg-6">
                         <ul class="check-lists">
-                            <li>Finance Services</li>
-                            <li>Consumer Markets</li>
+                            <li>Accompagnement</li>
+                            <li>Autonomisation</li>
                         </ul>
                     </div>
                 </div>
@@ -33,12 +37,12 @@
                         </div>
                         <div class="services-content">
                             <h5 class="title"> Call to ask any question </h5>
-                            <p class="services-txt"><a href="tel:+0885898745">(+088) 589-8745</a></p>
+                            <p class="services-txt"><a href="tel:+0885898745">(+243) 821 434 499</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="btn-part mt-40">
-                    <a class="readon slide-started more" href="about.html"><span>Get Started</span></a>
+                    <a class="readon slide-started more" href="#"><span>voir plus</span></a>
                 </div>
             </div>
             <div class="col-lg-6">
